f2dc15bb4f8c04d895aa627362eef315c1dc4672
Pape <princecurry44@gmail.com>
2025-03-20 13:33:56 -0400

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5009;
+const PORT = process.env.PORT || 5020;
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
